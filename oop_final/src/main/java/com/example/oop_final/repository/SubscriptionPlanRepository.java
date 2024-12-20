3f13486e24f63a127e69803bffb76bc5245d13a0
markodesu <mariiam.raiimzhanova_2027@ucentralasia.org>
2024-12-20 12:52:41 +0600

    Delete oop_final directory

--- oop_final/src/main/java/com/example/oop_final/repository/SubscriptionPlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.oop_final.repository;
-
-import com.example.oop_final.model.SubscriptionPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Long> {
-}
--- oop_final/src/main/java/com/example/oop_final/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.oop_final.repository;
-
-import com.example.oop_final.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
